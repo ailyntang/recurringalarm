21fa4adf3643a0d42b16a47b1dcc93c023f5db8c
Ai-Lyn Tang <ai-lyntang@192-168-1-5.tpgi.com.au>
2018-01-06 13:32:22 +1100

    Initial project setup

---
+++ RecurringAlarms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecurringAlarms
+//
+//  Created by Ai-Lyn Tang on 6/1/18.
+//  Copyright © 2018 Ai-Lyn Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
